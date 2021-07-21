95beb385d26f7f57335673ea1c5467270aef649b
Justin Stephens <jstephens2020@outlook.com>
2021-07-21 23:22:23 +0000

    Model now driven by controller script

--- godot/scripts/joint-driver.gd
+++ godot/scripts/joint-driver.gd
@@ -20,30 +20,26 @@ export var br: Vector3
 export var bl: Vector3
 export var neck: float
 
-# Reference to joint Spatial nodes
-var joints: Array
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	joints.append_array([
-		$"shoulder-fl",
-		$"shoulder-fl/hip",
-		$"shoulder-fl/hip/knee",
-		$"shoulder-fr",
-		$"shoulder-fr/hip",
-		$"shoulder-fr/hip/knee",
-		$"shoulder-br",
-		$"shoulder-br/hip",
-		$"shoulder-br/hip/knee",
-		$"shoulder-bl",
-		$"shoulder-bl/hip",
-		$"shoulder-bl/hip/knee",
-		$"body/neck"
-	])
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Update joint positions here
+	$"neck/shoulder-fl".rotation 			= Vector3(0, PI, fl.x)
+	$"neck/shoulder-fl/hip".rotation		= Vector3(-fl.y, 0, 0)
+	$"neck/shoulder-fl/hip/knee".rotation 	= Vector3(fl.z, 0, 0)
+	$"neck/shoulder-fr".rotation 			= Vector3(0, PI, PI - fr.x)
+	$"neck/shoulder-fr/hip".rotation		= Vector3(fr.y, 0, 0)
+	$"neck/shoulder-fr/hip/knee".rotation 	= Vector3(-fr.z, 0, 0)
+	$"body/shoulder-br".rotation 			= Vector3(0, PI, PI - br.x)
+	$"body/shoulder-br/hip".rotation		= Vector3(br.y, 0, 0)
+	$"body/shoulder-br/hip/knee".rotation 	= Vector3(-br.z, 0, 0)
+	$"body/shoulder-bl".rotation 			= Vector3(0, PI, bl.x)
+	$"body/shoulder-bl/hip".rotation		= Vector3(-bl.y, 0, 0)
+	$"body/shoulder-bl/hip/knee".rotation 	= Vector3(bl.z, 0, 0)
+	$"neck".rotation						= Vector3(0, 0, neck)
 	pass
