71fa11d729eb8163d5be4c52bc8d24d2ca918419
Robert-Mones <robertmones42@gmail.com>
2022-02-05 02:08:55 -0500

    Mostly benchmarked the I2C peripherals

--- Arduino/Teensy 4.1 I2C Benchmark/src/main.cpp
+++ Arduino/Teensy 4.1 I2C Benchmark/src/main.cpp
@@ -1,14 +1,41 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 // Display
-#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
 
-Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire2, -1, 400000L, 400000L);
+// IMU
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
-void writeDisplay() {
+// Servos
+#include <Adafruit_PWMServoDriver.h>
+
+// Current Sensor
+#include <Adafruit_INA260.h>
+
+// Threading
+#include <TeensyThreads.h>
+
+/* Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire2, -1, 400000L, 400000L);
+
+void displaySetup() {
+  if(display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setFont();
+
+    display.clearDisplay();
+    display.display();
+  } else {
+    Serial.printf("Display initialization failed.\n");
+  }
+}
+
+void displayLoop() {
   uint32_t t1 = 0, t2 = 0, t3 = 0, t4 = 0;
 
   t1 = micros();
@@ -26,10 +53,74 @@ void writeDisplay() {
   display.display();
   t4 = micros();
 
-  Serial.printf("Timing\t%lu\t%lu\t%lu\n", t2-t1, t3-t2, t4-t3);
+  //Serial.printf("Timing\t%lu\t%lu\t%lu\n", t2-t1, t3-t2, t4-t3);
+} */
+
+/* Adafruit_BNO055 bno = Adafruit_BNO055(55, 40U, &Wire);
+
+void IMUSetup() {
+  if(bno.begin()) {
+    bno.setExtCrystalUse(true);
+  } else {
+    Serial.printf("IMU begin failed\n");
+  }
 }
 
-/*  Results:
+void IMULoop() {
+  uint32_t t1 = 0, t2 = 0;
+
+  t1 = micros();
+  sensors_event_t event; 
+  bno.getEvent(&event);
+  t2 = micros();
+  
+  // Display the floating point data
+  Serial.printf("X: %.4f\tY: %.4f\tZ: %.4f\tt: %lu\n",
+    event.orientation.x, event.orientation.y, event.orientation.z, t2-t1);
+} */
+
+/* Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(64U, Wire1);
+
+void servoSetup() {
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(50);
+}
+
+void servoLoop() {
+  uint32_t t1, t2;
+
+  t1 = micros();
+  for(int i = 0; i < 16; i++) {
+    pwm.setPWM(i, 0, 300);
+  }
+  t2 = micros();
+
+  Serial.printf("Servo time: %lu\n", t2-t1);
+} */
+
+/* Adafruit_INA260 ina260 = Adafruit_INA260();
+
+void currentSetup() {
+  if(!ina260.begin(64U, &Wire1)) {
+    Serial.printf("Couldn't find current sensor.\n");
+  }
+}
+
+void currentLoop() {
+  float current, voltage, power;
+  uint32_t t1, t2;
+
+  t1 = micros();
+  current = ina260.readCurrent();
+  voltage = ina260.readBusVoltage();
+  power = ina260.readPower();
+  t2 = micros();
+
+  Serial.printf("Power: %.2f\t%.2f\t%.2f\t%lu\n", current, voltage, power, t2-t1);
+} */
+
+/*  Unthreaded SSD1306:
  *    100kHz w/ 2kO:   99kHz actual   100700us
  *    400kHz:         338kHz actual    29600us
  *    400kHz w/ 2kO:  375kHz actual    26700us
@@ -39,23 +130,32 @@ void writeDisplay() {
  *    800kHz w/ 330O: 400kHz actual    25000us -- definitely software limited by SSD1306 chip
  */
 
-void setup() {
-  if(display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setFont();
+/*  Unthreaded BNO055:
+ *    100kHz w/ 1kO:  100kHz actual   1000-2000us
+ */
 
-    display.clearDisplay();
-    display.display();
-  } else {
-    Serial.printf("Display initialization failed.\n");
-  }
+/*  Unthreaded PCA9685:
+ *    100kHz w/ 1kO:   90kHz actual   1700us
+ */
+
+/*  Unthreaded INA260:
+ *    100kHz w/ 0kO:   ??kHz actual   1500us
+ */
+
+void setup() {
+  // displaySetup();
+  // IMUSetup();
+  // servoSetup();
+  // currentSetup();
 
   delay(3000);
 }
 
 void loop() {
-  writeDisplay();
+  // displayLoop();
+  // IMULoop();
+  // servoLoop();
+  // currentLoop();
 
-  delay(3000);
+  delay(100);
 }
